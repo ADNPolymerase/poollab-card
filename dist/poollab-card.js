02a9086f29326ca405976dbd5f0919f97eab128b
ADNPolymerase <111017981+ADNPolymerase@users.noreply.github.com>
2026-07-26 20:12:54 +0200

    v0.6.0

--- dist/poollab-card.js
+++ dist/poollab-card.js
@@ -1,4 +1,4 @@
-const CARD_VERSION = "0.6.0-beta.1";
+const CARD_VERSION = "0.6.0";
 const _D = String.fromCharCode(176);
 const _e = String.fromCharCode(233);
 const _eg = String.fromCharCode(232);
